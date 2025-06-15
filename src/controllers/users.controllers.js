1abfc3f559ce82153d602fbc29835db45e432cb5
mdselimme <mdselimdev@gmail.com>
2025-06-15 16:07:19 +0600

    add login system

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -12,7 +12,7 @@ userRouter.post('/register', async (req, res) => {
         // find user if exists 
         const user = await UserRegister.findOne({ email: email });
         if (user) {
-            return res.status(400).send("User Already Exits. Please sign in");
+            return res.status(400).send({ message: "User Already Exits. Please sign in" });
         } else {
             // make password bycrypt hashPassword 
             const salt = await bcrypt.genSalt(10);
@@ -29,6 +29,34 @@ userRouter.post('/register', async (req, res) => {
     } catch (error) {
         return res.status(400).json({ message: error.message });
     }
+});
+
+// Login User
+userRouter.post('/login', async (req, res) => {
+    try {
+        // destructure properties from body 
+        const { email, password } = req.body;
+        // find user if exists 
+        const user = await UserRegister.findOne({ email: email });
+        if (!user) {
+            return res.status(400).send({ message: "Invalid Users. Please sign in first." });
+        };
+
+        // password matching with bycrypt 
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        // is password didn't match 
+        if (!isMatch) {
+            return res.status(400).send({ message: "Invalid Password. Please sign in with right password." });
+        }
+        // json web token create 
+        const token = jwt.sign({ id: user._id }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+
+        return res.status(201).json({ token, message: "User Logged in Successfully", user: { id: user._id, name: user.fullName } });
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    };
 });
 
 
